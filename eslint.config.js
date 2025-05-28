6421477a46700aaab6c3b022f0f5fa210ee5fd84
Markus Tacker <Markus.Tacker@NordicSemi.no>
2025-05-29 00:24:02 +0200

    feat: turn off @typescript-eslint/require-await
    This is rarely actually a problem and can lead to inconsistent code style.

--- eslint.config.js
+++ eslint.config.js
@@ -77,7 +77,7 @@ export default tseslint.config(
 			'@typescript-eslint/no-unsafe-return': ['warn'],
 			'@typescript-eslint/no-unsafe-argument': ['warn'],
 			'@typescript-eslint/no-unsafe-call': ['warn'],
-			'@typescript-eslint/require-await': ['warn'],
+			'@typescript-eslint/require-await': ['off'],
 			'@typescript-eslint/no-misused-promises': ['warn'],
 			'@typescript-eslint/no-base-to-string': ['warn'],
 			'@typescript-eslint/consistent-type-imports': ['error'],
